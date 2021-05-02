d58d053d0b1c5bd9878db9717b4285b7364747fc
Norbert Schnell <norbert.schnell@hs-furtwangen.de>
2021-05-02 12:16:25 +0200

    code cosmetics

--- drumpads/scripts.js
+++ drumpads/scripts.js
@@ -1,20 +1,20 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 let audioContext = null;
 
-const sounds = ['bd.wav', 'sd.wav', 'ch.wav', 'oh.wav'];
-const buffers = [];
+const soundsFileNames = ['bd.wav', 'sd.wav', 'ch.wav', 'oh.wav'];
+const audioBuffers = [];
 
 window.addEventListener('mousedown', onButton);
 window.addEventListener('touchstart', onButton);
 
-// load audio buffers
-for (let i = 0; i < sounds.length; i++) {
+// load audio buffers (samples)
+for (let i = 0; i < soundsFileNames.length; i++) {
   const request = new XMLHttpRequest();
   request.responseType = 'arraybuffer';
-  request.open('GET', sounds[i]);
+  request.open('GET', soundsFileNames[i]);
   request.addEventListener('load', () => {
     const ac = new AudioContext();
-    ac.decodeAudioData(request.response, (buffer) => buffers[i] = buffer);
+    ac.decodeAudioData(request.response, (buffer) => audioBuffers[i] = buffer);
   });
 
   request.send();
@@ -22,16 +22,17 @@ for (let i = 0; i < sounds.length; i++) {
 
 // play buffer by index
 function playSound(index) {
+  // create audio context on first button and keep it
   if (audioContext === null)
     audioContext = new AudioContext();
 
   const source = audioContext.createBufferSource();
   source.connect(audioContext.destination);
-  source.buffer = buffers[index];
+  source.buffer = audioBuffers[index];
   source.start(audioContext.currentTime);
 }
 
-// play sample
+// play audio buffer (sample)
 function onButton(evt) {
   const target = evt.target;
   const index = target.dataset.index;
